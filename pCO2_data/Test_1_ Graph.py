239e8bdc9d2e457e23bae5005b369da23a6bc84b
isabellayeung <101486851+isabellayeung@users.noreply.github.com>
2023-06-19 19:25:56 -0400

    41

--- pCO2_data/Test_1_ Graph.py
+++ pCO2_data/Test_1_ Graph.py
@@ -30,23 +30,30 @@ plt.figure().set_figwidth(60)
 
 fig, ax1 = plt.subplots()
 p1 = ax1.plot(x, ty, color = 'b', linestyle = 'solid', label = "Temperature (C)")
+
+ax1.xaxis.set_ticks(np.arange(math.trunc(x[0]), math.trunc(x[-1]), 1))
+ax1.set_xticklabels(ax1.get_xticks(), rotation = 90)
+
 ax1.set_ylabel("Temperature (C)")
+ax1.set_xlabel("Dates")
+ax1.yaxis.label.set_color(p1[0].get_color())
+
 ax2 = ax1.twinx()
 p2 = ax2.plot(x, cy, color = 'r', linestyle = 'solid', label = "CO2")
 ax2.set_ylabel("CO2")
+ax2.yaxis.label.set_color(p2[0].get_color())
+
 ax3 = ax1.twinx()
 p3 = ax3.plot(x, by, color = 'g', linestyle = 'solid', label = "Battery Voltage")
 ax3.set_ylabel("Battery Voltage")
-
 ax3.spines["right"].set_position(("outward", 60))
-
+ax3.yaxis.label.set_color(p3[0].get_color())
 
 #plt.plot(x, ty, color = 'b', linestyle = 'solid', label = "Temperature (C)")
 #plt.plot(x, cy, color = 'r', linestyle = 'solid', label = "CO2")
 #plt.plot(x, by, color = 'g', linestyle = 'solid', label = "Battery Voltage")
   
-plt.xticks(np.arange(math.trunc(x[0]), math.trunc(x[-1]), 1), rotation = 90)
-plt.xlabel('Dates')
+
 plt.title('pCO2 Data', fontsize = 20)
 plt.grid(True)
 plt.legend(handles=p1+p2+p3)
