8c7477d253ba2eba283c2256d430d4b980e080d9
isabellayeung <101486851+isabellayeung@users.noreply.github.com>
2023-06-25 16:10:36 -0400

    z

--- pCO2_data/Test_1_ Graph.py
+++ pCO2_data/Test_1_ Graph.py
@@ -5,18 +5,22 @@ import numpy as np
 import csv
 import math
 
+x = None
+ty = None
+cy = None
+by = None
+
+# Used to hold data from csv file  
+xData = []      # Dates
+tyData = []     # Temperature
+cyData = []     # CO2
+byData = []     # Battery Voltage
+
+numofLines = 0
 
 # Sets the graph layout colors and style
 plt.style.use('default')
 
-# Used to hold data from csv file  
-x = []      # Dates
-ty = []     # Temperature
-cy = []     # CO2
-by = []     # Battery Voltage
-
-numofLines = 0
- 
 with open('C:\\Users\\isabe\\Source\\Repos\\icyeung\\pCO2-DataTrue\\pCO2_data\\completeData.csv','r') as csvfile:
     lines = csv.reader(csvfile, delimiter='\t')
     for row in lines:
@@ -24,13 +28,16 @@ with open('C:\\Users\\isabe\\Source\\Repos\\icyeung\\pCO2-DataTrue\\pCO2_data\\c
         # Checks if time entry has corresponding Temperature, CO2, and Battery Voltage
         # If not, does not include data point in graph
         if not row[1] == "" and not row[2] == "" and not row[3] == "" and numofLines > 0:
-            x.append(float(row[0]))
-            ty.append(float(row[1]))
-            cy.append(float(row[2]))
-            by.append(float(row[3]))
+            xData.append(float(row[0]))
+            tyData.append(float(row[1]))
+            cyData.append(float(row[2]))
+            byData.append(float(row[3]))
             numofLines += 1
         elif numofLines == 0:
             numofLines += 1
+
+
+
 
 # Makes graph wider so Dates can be viewed properly
 plt.figure().set_figwidth(60)
@@ -38,10 +45,13 @@ plt.figure().set_figwidth(60)
 # Allows for more than one set of data to be plotted
 
 # Temperature plot
+x = xData[::10]
+ty = tyData[::10]
 fig, ax1 = plt.subplots()
 p1 = ax1.plot(x, ty, color = 'b', linestyle = 'solid', label = "Temperature (C)")
 
 # Sets x-axis as Dates
+
 ax1.xaxis.set_ticks(np.arange(math.trunc(x[0]), math.trunc(x[-1]), 1))     # Step size of 1
 ax1.set_xticklabels(ax1.get_xticks(), rotation = 90)        # Rotates dates to be perpendiculat to x-axis
 
@@ -53,11 +63,13 @@ ax1.yaxis.label.set_color(p1[0].get_color())
 
 # CO2 plot
 ax2 = ax1.twinx()
+cy = cyData[::10]
 p2 = ax2.plot(x, cy, color = 'r', linestyle = 'solid', label = "CO2")
 ax2.set_ylabel("CO2")
 ax2.yaxis.label.set_color(p2[0].get_color())
 
 # Battery Voltage plot
+by = byData[::10]
 ax3 = ax1.twinx()
 p3 = ax3.plot(x, by, color = 'g', linestyle = 'solid', label = "Battery Voltage")
 ax3.set_ylabel("Battery Voltage")
