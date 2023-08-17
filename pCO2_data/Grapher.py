d65d17495e482ac31fae45aeba26207a83933834
Isabella Yeung <icyeung3@mit.edu>
2023-08-17 16:57:58 -0400

    weather data merging (scaled) does not work else formatted correctly

--- pCO2_data/Grapher.py
+++ pCO2_data/Grapher.py
@@ -421,10 +421,18 @@ tideDFscaled = pd.DataFrame({"Date": tidDateTrue, "Height (ft)": scaledValueTide
 
 mergedDFscaled = pd.merge_asof(pco2DFscaled, tideDFscaled, on="Date", tolerance=pd.Timedelta("1m"), allow_exact_matches=False)
 
+mergedDFscaled = pd.merge(mergedDFscaled, weatherDFscaled, on='Date')
+
 mergedDFscaled['Year'] = mergedDFscaled['Date'].dt.year
 mergedDFscaled['Month'] = mergedDFscaled['Date'].dt.month
 mergedDFscaled['Day'] = mergedDFscaled['Date'].dt.day
-mergedDFscaled['Time'] = mergedDFscaled['Date'].dt.time
+mergedDFscaled['Hour'] = mergedDFscaled['Date'].dt.hour
+mergedDFscaled['Minute'] = mergedDFscaled['Date'].dt.minute
+
+
+mergedDFscaled.loc[mergedDFscaled.Minute != 0, 'Hour'] += (mergedDFscaled.Minute/60)
+
+
 
 mergedDFscaled.to_excel("merge_tester_scaled.xlsx")
 print(mergedDFscaled)
