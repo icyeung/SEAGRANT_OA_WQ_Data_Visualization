5c611aa3f62c61bf7e7bcbf888c1d027378ebf4f
Isabella Yeung <icyeung3@mit.edu>
2023-08-24 01:14:59 -0400

    changed cov from displaying in percent to decimal

--- pCO2_data/Battery_Grapher.py
+++ pCO2_data/Battery_Grapher.py
@@ -5,50 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-# Used to find location of specified file within Python code folder
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-dateList = []
-co2List = []
-batteryList = []
-cvList = []
-
-with open(os.path.join(__location__,'pco2_Battery_Data.csv'), 'r') as csvfile:
-    data=[tuple(line) for line in csv.reader(csvfile)]
-    
-print(data)
-
-for row in data[1:]:
-    dateList.append(row[0])
-    co2List.append(float(row[1]))
-    batteryList.append(float(row[2]))
-    
-    if len(co2List) > 1:
-        co2CV = np.std(co2List,ddof=1) / np.mean(co2List) * 100
-        cvList.append(co2CV)
-    else:
-        cvList.append(float(0))
-    
-
-pco2_Battery_DF = pd.DataFrame({"Date": dateList, "CO2": co2List, "Battery": batteryList, 
-                                "Coefficient of Variation": cvList})
-
-
-print(pco2_Battery_DF)
-
-plt.plot(batteryList, cvList)
-plt.axis([max(batteryList), min(batteryList), min(cvList), max(cvList)])
-# new_list = range(math.floor(min(batteryList)), math.floor(math.ceil(max(batteryList))+1))
-# plt.xticks(new_list)
-# plt.xticks(rotation ='vertical')
-plt.xlabel('Battery Level')
-plt.ylabel('Coefficient of Variation')
-plt.title('Coefficient of Variation vs Battery', fontsize = 15)
-plt.grid(True)
-plt.show()
-
-
-plt.savefig('2021_pco2CV_vs_Battery.png')
 
 '''
 
@@ -68,3 +24,55 @@ repeats until the no more rows to add
 what if data row is stored in a tuple
 
 '''
+
+
+# Used to find location of specified file within Python code folder
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Holds data
+dateList = []
+co2List = []
+batteryList = []
+cvList = []
+
+# Saves csv as tuple for every row
+with open(os.path.join(__location__,'pco2_Battery_Data.csv'), 'r') as csvfile:
+    data=[tuple(line) for line in csv.reader(csvfile)]
+    
+# For every row in tuple, coefficient of variation is calculated and appended to holder list 
+# as each co2 measurement is extracted from tuple
+for row in data[1:]:
+    dateList.append(row[0])
+    co2List.append(float(row[1]))
+    batteryList.append(float(row[2]))
+    
+    # Skips first co2 value as DoF would be < 1
+    # Substitutes first coefficient of variation value calculation with 0
+    if len(co2List) > 1:
+        co2CV = np.std(co2List,ddof=1) / np.mean(co2List)
+        cvList.append(co2CV)
+    else:
+        cvList.append(float(0))
+    
+# Creates dataframe of data used for calculation & graph
+pco2_Battery_DF = pd.DataFrame({"Date": dateList, "CO2": co2List, "Battery": batteryList, 
+                                "Coefficient of Variation": cvList})
+
+
+print(pco2_Battery_DF)
+
+# Creates graph
+plt.plot(batteryList, cvList)
+plt.axis([max(batteryList), min(batteryList), min(cvList), max(cvList)])
+# new_list = range(math.floor(min(batteryList)), math.floor(math.ceil(max(batteryList))+1))
+# plt.xticks(new_list)
+# plt.xticks(rotation ='vertical')
+plt.xlabel('Battery Level')
+plt.ylabel('Coefficient of Variation')
+plt.title('Coefficient of Variation vs Battery', fontsize = 15)
+plt.grid(True)
+plt.show()
+
+# Saves figure as png
+plt.savefig('2021_pco2CV_vs_Battery.png')
+
