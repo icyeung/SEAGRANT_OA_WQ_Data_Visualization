3ac2d5ecfa592e2f36505dcb8eb7234ebabb6f0a
isabellayeung <101486851+isabellayeung@users.noreply.github.com>
2024-01-06 14:23:25 -0500

    update

---
+++ Conductivity/Buzzards_Bay_Data/buzzard_bay_data_grapher.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'Salinity_Carolina_9-28-21.csv'),'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+      
+        # Checks if time entry has corresponding Time and Verified Measurement
+        # If not, does not include data point in graph
+        if not row[0] == "-" and not row[1] == "-" and not row[2] == "-" and not row[0] == "" and not row[1] == "" and not row[2] == "" and numofLinesS > 0:
+           
+            numofLinesS += 1
+        elif numofLinesS <= 0:
+            numofLinesS += 1
