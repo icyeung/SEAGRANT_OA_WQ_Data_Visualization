10db626ca1ed2c4444241e9e01825474a8bc4576
Isabella Yeung <icyeung3@mit.edu>
2024-01-24 16:42:05 -0500

    123

--- Castaway/castaway_data_filler.py
+++ Castaway/castaway_data_filler.py
@@ -56,25 +56,9 @@ main()
 # program goes through table line by line
 
 def castawayRetriever (file_logger_input, start_date, end_date):
-    # Used to find location of specified file within Python code folder
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+   # Used to find location of specified file within Python code folder
+   __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-
-    # Used in taking out empty values from salinity data
-    numofLinesS = -1
-
-    # Takes out empty values in salinity data set
-    with open(os.path.join(__location__, 'Salinity_Carolina_12-10-21.csv'),'r') as csvfile:
-        lines = csv.reader(csvfile, delimiter=',')
-        for row in lines:
-      
-            # Checks if time entry has corresponding Time and Verified Measurement
-            # If not, does not include data point in graph
-            if not row[0] == "-" and not row[1] == "-" and not row[2] == "-" and not row[0] == "" and not row[1] == "" and not row[2] == "" and numofLinesS > 0:
-                salDate.append(row[0])
-                print(row[0])
-                condData.append(float(row[1]))
-                condTempData.append(float(row[2]))
-                numofLinesS += 1
-            elif numofLinesS <= 0:
-                numofLinesS += 1
+   logger_df = pd.read_csv(os.path.join(__location__, file_logger_input))
+  
+   print(logger_df)
