065ed621791922605bf96b1a60aa01cbbadbefa2
Isabella Yeung <icyeung3@mit.edu>
2024-02-24 11:50:56 -0500

    created formatting for year series graphing

---
+++ Graphing_Across_Years/year_analyzer_grapher.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+
+# trial 1- let's have each type of data separated from each other
+# how should this work
+
+# PCO2
+# get one file of pco2 processed data for each year
+# input each year as a series
+# plot temperature and co2
+# potential issues: need to figure out how to make the x axis equal, graph on same y axis scale
+
+# pH
+# get one file of pH processed data for each year
+# input each year as a series
+# plot temperature and pH
+# potential issues: need to figure out how to make the x axis equal, graoh on same y axis scale
+
+# conductivity
+# separate the locations
+# graph the overlaps of the data we have with the buzzard's bay data
+# - to do this: need to compare dates, if same, adds data to dataframe
+# should use salinity? (check values of salinity because they look weird:/)
+# indicates the places where there are castaway meaasurements (dot plot)
+# potential issues: need to figure out how to make x axis equal, need to make the data line up and account for breaks, need to ensure overlap program works, 
+# graph on same y-axis scale
+
+
+
+# trial 2- let's mush everything together and have toggle options on what to display and what not to
+# will most likely look too squished to be of any real help
+# toggle option will be important
+# how the hell do i fit everything on same scale axes
+# having date parameters will also be good?
+
+
+
+
---
+++ Graphing_Across_Years/year_analyzer_grapher_conductivity.py
@@ -0,0 +1,5 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
