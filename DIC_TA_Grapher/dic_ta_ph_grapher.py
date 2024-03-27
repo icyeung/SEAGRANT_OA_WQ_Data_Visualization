a516b360f9c3abd7eae39108b9f185e78a4618a5
isabellayeung <101486851+isabellayeung@users.noreply.github.com>
2024-03-27 16:39:07 -0400

    edited

--- DIC_TA_Grapher/dic_ta_ph_grapher.py
+++ DIC_TA_Grapher/dic_ta_ph_grapher.py
@@ -10,22 +10,33 @@ import matplotlib.dates as mdates
 # Create graph overlay of water sampling date and pco2 or ph
 # y-axis would be total alkalinity/DIC and calculated pco2/ph 
 # maybe use 2020 as an example (not 2022 as it has the errors)
+# will have to use 2021 as there is no water sample data for HAR besides 2021 
+
 
 # use data from 2017-2022 mwra
 # tco2=dic
 # ta
 # out vlues are calculated
+#Stat_ID = HAR
 
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-pco2_section_data = pd.read_csv(os.path.join(__location__, "pco2_2022_subsection_oct.csv"))
+measured_pH_data_df = pd.read_csv("C:\\Users\\isabe\\source\\repos\\SEAGRANT_OA_WQ_Data_Visualization\\Graphing_Across_Years\\pH\\pH_Data_2021_Compiled_Monthly.csv")
 
-NOAA_tidal_data = pd.read_csv(os.path.join(__location__, "NOAA_Tidal_HL_2022_Monument_Beach.csv"))
+MWRA_data = pd.read_csv("C:\\Users\\isabe\\source\\repos\\SEAGRANT_OA_WQ_Data_Visualization\\MWRA\\MWRA_Data\\MWRA_TA_DIC_2017_to_2022_v20240325.csv")
 
+MWRA_trunc_df = pd.DataFrame()
+MWRA_trunc_df = pd.DataFrame(data=MWRA_trunc_df, columns=MWRA_data.columns)
 
-# Need to cut out section of NOAA_tidal_data that fits with date range of pco2_section_data
-# Program where you input time period to get overlay instead of having a manual overlay? probably
+for index in range(0, len(MWRA_data)):
+#print("A:", indexA)
+    if ((MWRA_data.loc[index, "STAT_ID"])) == "HAR":
+        print("yay the date works")
+        new_row = MWRA_data.loc[index].copy()
+        MWRA_trunc_df.loc[index] = new_row
+        print("yay")
+
 
 # Cuts out data that do not fit within desired date range
 # Input: dataframe, start date in "mm-dd-yyyy", end date in "mm-dd-yyyy"
@@ -41,8 +52,11 @@ def commonDataRange(data_df, start_date, end_date):
     invalid_date_index_list = []
     logger_date_index = 0
 
-    logger_dates_list = data_df["Date"].tolist()
+    logger_dates_list = data_df["PROF_DATE_TIME_LOCAL"].tolist()
     for date in logger_dates_list:
+        date = str(date)
+        date = date.split(" ")[0]
+        print(date)
         m1, d1, y1 = [int(date_part) for date_part in date.split("/")]
         date1 = dt(y1, m1, d1)
       
@@ -60,11 +74,12 @@ def commonDataRange(data_df, start_date, end_date):
     return data_df
 
 
-NOAA_fitted_data = commonDataRange(NOAA_tidal_data, "09-30-2022", "11-01-2022")
-print(commonDataRange(NOAA_tidal_data, "09-30-2022", "11-01-2022"))
+MWRA_fitted_data = commonDataRange(MWRA_trunc_df, "01-01-2021", "12-31-2021")
+print(MWRA_fitted_data)
 
 print("yay, time is done?")
 
+'''
 def timeConverterto24(time):
     ending = time.split(" ")[-1]
     time_number = time.split(" ")[0]
@@ -78,7 +93,7 @@ def timeConverterto24(time):
     return converted_time_dt
 
 NOAA_tidal_data_time_converted_list = []
-for time in NOAA_fitted_data["Time"]:
+for time in MWRA_fitted_data["Time"]:
     NOAA_tidal_data_time_converted_list.append(timeConverterto24(time))
 #print("time", NOAA_tidal_data_time_converted_list)
 
@@ -96,96 +111,94 @@ for index in range(0, len(NOAA_tidal_data_date_converted_list)):
 NOAA_fitted_data["DateTime"] = NOAA_tidal_data_datetime_combined_list
 print(NOAA_fitted_data)
 print("another part done?")
+'''
 
-# pCO2 2022 data section is good
-pco2_data = pco2_section_data["CO2"]
-pco2_date = pco2_section_data["Date"]
-pco2_date_revised = []
-for date in pco2_date:
+
+# pH 2022 data section is good
+measured_pH_data = measured_pH_data_df["pH"]
+pH_date = measured_pH_data_df["Date"]
+pH_date_revised = []
+for date in pH_date:
     date_no_year = '{:%m-%d %H:%M:%S}'.format(dt.strptime(date, '%Y-%m-%d %H:%M:%S'))
     date_no_year = str(date_no_year)
     dt_date_no_year = dt.strptime(date_no_year, "%m-%d %H:%M:%S")
-    pco2_date_revised.append(dt_date_no_year)
+    pH_date_revised.append(dt_date_no_year)
 
 
-# Need to decipher NOAA data
-NOAA_date = NOAA_fitted_data["DateTime"]
-NOAA_date_revised = []
-for date in NOAA_date:
-    date_no_year = '{:%m-%d %H:%M:%S}'.format(dt.strptime(str(date), '%Y-%m-%d %H:%M:%S'))
+
+
+# Need to decipher MWRA data
+TA_data = MWRA_fitted_data["TA in (mmol/kgSW)"]
+DIC_data = MWRA_fitted_data["TCO2 in (mmol/kgSW)"]
+cal_pH_data = MWRA_fitted_data["pH out"]
+MWRA_date = MWRA_fitted_data["PROF_DATE_TIME_LOCAL"]
+MWRA_date_revised = []
+
+
+revised_MWRA_list = []
+for date in MWRA_date:
+    time = date.split(" ")[1]
+    day = date.split(" ")[0]
+    hour = time.split(":")[0]
+    minute = time.split(":")[1]
+    hour = str(hour)
+    if len(hour) == 1:
+        hour = "0" + hour
+        new_time = hour + ":" + minute
+        new_date = day + " " + new_time
+        revised_MWRA_list.append(new_date)
+    else:
+        revised_MWRA_list.append(date)
+
+
+for date in revised_MWRA_list:
+    print(date)
+    date_no_year = '{:%m-%d %H:%M}'.format(dt.strptime(date, '%m/%d/%Y %H:%M'))
     date_no_year = str(date_no_year)
-    dt_date_no_year = dt.strptime(date_no_year, "%m-%d %H:%M:%S")
-    NOAA_date_revised.append(dt_date_no_year)
-
-
-# Decipher NOAA high/low data
-# If high, then puts in time to high_list
-# If low, then puts in time to low_list
-# High_list -> star points?
-# Low_list -> triangle points?
-    
-NOAA_date_high = []
-NOAA_date_low = []
-NOAA_data_high = []
-NOAA_data_low = []
-for index in range(0, len(NOAA_date)):
-    type = NOAA_fitted_data.loc[index, "High/Low"]
-    if type == "H":
-        NOAA_date_high.append(NOAA_fitted_data.loc[index, "DateTime"])
-        NOAA_data_high.append(NOAA_fitted_data.loc[index, "Pred(cm)"])
-    if type == "L":
-        NOAA_date_low.append(NOAA_fitted_data.loc[index, "DateTime"])
-        NOAA_data_low.append(NOAA_fitted_data.loc[index, "Pred(cm)"])
-
-
-NOAA_date_high_revised = []
-for date in NOAA_date_high:
-    date_no_year = '{:%m-%d %H:%M:%S}'.format(dt.strptime(str(date), '%Y-%m-%d %H:%M:%S'))
-    date_no_year = str(date_no_year)
-    dt_date_no_year = dt.strptime(date_no_year, "%m-%d %H:%M:%S")
-    NOAA_date_high_revised.append(dt_date_no_year)
-
-
-NOAA_date_low_revised = []
-for date in NOAA_date_low:
-    date_no_year = '{:%m-%d %H:%M:%S}'.format(dt.strptime(str(date), '%Y-%m-%d %H:%M:%S'))
-    date_no_year = str(date_no_year)
-    dt_date_no_year = dt.strptime(date_no_year, "%m-%d %H:%M:%S")
-    NOAA_date_low_revised.append(dt_date_no_year)
-
+    dt_date_no_year = dt.strptime(date_no_year, "%m-%d %H:%M")
+    MWRA_date_revised.append(dt_date_no_year)
 
 
 fig, ax1 = plt.subplots(figsize=(14,7))
-p1 = ax1.plot(pco2_date_revised, pco2_data, color = "b", linestyle = 'solid', label = '2022', linewidth=0.75)
-
+p1 = ax1.plot(pH_date_revised, measured_pH_data, color = "b", linestyle = 'solid', label = 'Measured pH', linewidth=0.75)
+p4 = ax1.scatter(MWRA_date_revised, cal_pH_data, color = 'c', marker = "D", label = "Calculated pH")
 # Sets x-axis as Dates
 date_form = DateFormatter("%m-%d")
 ax1.xaxis.set_major_formatter(date_form)
-#ax1.xaxis.set_major_locator(mdates.WeekdayLocator(interval = 2))     # Displays x-axis label every 14 days
-ax1.xaxis.set_major_locator(mdates.DayLocator(interval = 2))       # Indicates each day (without label) on x-axis
+ax1.xaxis.set_major_locator(mdates.WeekdayLocator(interval = 2))     # Displays x-axis label every 14 days
+#ax1.xaxis.set_major_locator(mdates.DayLocator(interval = 2))       # Indicates each day (without label) on x-axis
     
 # Sets axis labels and changes font color of "pco2" label for easy viewing
-ax1.set_ylabel("pCO2")
+ax1.set_ylabel("Measured pH")
 ax1.set_xlabel("Dates (MM-DD)")
-ax1.yaxis.label.set_color("k")  
+ax1.yaxis.label.set_color("k")
+ax1.legend()  
 
-# Graphs NOAA data
 ax2 = ax1.twinx()
-p2a = ax2.scatter(NOAA_date_high_revised, NOAA_data_high, color = 'g', marker = "*", label = 'Monument Beach- High Tide')
-p2b = ax2.scatter(NOAA_date_low_revised, NOAA_data_low, color = 'r', marker = "^", label = 'Monument Beach- Low Tide')
-ax2.set_ylabel("Tide Height (cm)")
- 
+p2 = ax2.scatter(MWRA_date_revised, TA_data, color = 'g', marker = "*", label = 'TA')
+ax2.set_ylabel("TA (mmol/kgSW)")
+ax2.legend(loc = 'lower center')
+
+ax3 = ax1.twinx()
+p3 = ax3.scatter(MWRA_date_revised, DIC_data, color = 'r', marker = "^", label = "TCO2")
+ax3.set_ylabel("TOC2 (mmol/kgSW)")
+ax3.spines["right"].set_position(("outward", 60))
+ax3.legend(loc = 'lower center')
+
+'''
+handles, labels = ax1.get_legend_handles_labels()
+plt.figlegend(handles, labels, loc='upper center')
+'''
+
 
 # Sets title, adds a grid, and shows legend
 plt.tight_layout()
 plt.subplots_adjust(top=0.95)
-plt.title("pCO2 & High/Low Tidal Markers (2022)", loc='center')
+plt.title("pH: Calculated vs Measured (2021)", loc='center')
 plt.grid(True)
-plt.legend()
 
 my_path = os.path.dirname(os.path.abspath(__file__))
 
 # Saves without outliers graph to specified name in folder
-plt.savefig(my_path + '\\pco2_tidal_overlay_oct_2022_Graph_No_Outliers.png')
-
+plt.savefig(my_path + '\\ph_calculated_vs_measured_2021_Graph_No_Outliers.png')
 plt.show()
