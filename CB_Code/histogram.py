cf960d4930436ac8da87a4cf5e654abbaf60b244
carolina bastidas <bastidas@mit.edu>
2024-11-01 16:52:42 -0400

    playing2

--- CB_Code/histogram.py
+++ CB_Code/histogram.py
@@ -1,24 +1,37 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 #ph_data = pd.read_csv("C:\\Users\\isabe\\source\\repos\\icyeung\\SAMI_Data_SeaGrant\\Used_Data\\pH\\pH_2019_Complete_Annual_Data_NO.csv")
 ph_data = pd.read_csv("C:\\Users\\bastidas\\Dropbox (MIT)\\SG\\MAS\\Students\\UROPs\\2023\\Isabella_Yeung\\IY_github\\SEAGRANT_OA_WQ_Data_Visualization\\Used_Data\\pH\\pH_2019_Complete_Annual_Data_NO.csv")
 
+#   allows to check dataframe size and name of columns
+print(ph_data)
 
-# Access the 'Name' column
-T = ph_data[{'Temperature'}]
+#   allows to check column size
+#print(ph_data["Temperature C"])
 
-print(T)
-import matplotlib.pyplot as plt
-import numpy as np
+#   this does not work with this df
+#ph_data.head(3)
 
 '''
+#   Examples of plot
+plt.boxplot(ph_data["Temperature C"])
+plt.show()
+'''
+ph_data["Temperature C"].plot()
+plt.show()
+
+
 data = np.random.randn(1000)
 fig, ax = plt.subplots()
 ax.hist(data, color="purple")
 plt.show()
-'''
 
-data = T
+'''
+T = ph_data["pHConstSal"]
 fig, ax = plt.subplots()
-ax.hist(T, color="purple")
+ax.hist(T)
 plt.show()
+'''
